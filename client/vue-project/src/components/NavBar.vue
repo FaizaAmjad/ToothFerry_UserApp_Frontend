4e6a064ad37915ab812b5366b10115438b7a4a96
Faiza Amjad <faizaam@student.chalmers.se>
2023-11-09 18:45:46 +0100

    #13 Add navbar with bootstrap

---
+++ client/vue-project/src/components/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <RouterLink class="navbar-brand" to="/">Navbar</RouterLink>
+    <button class="navbar-toggler" type="button" data-coreui-toggle="collapse" data-coreui-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <RouterLink class="nav-link active" aria-current="page" to="/">Home</RouterLink>
+        </li>
+        <li class="nav-item">
+          <RouterLink class="nav-link" to="#">Features</RouterLink>
+        </li>
+        <li class="nav-item">
+          <RouterLink class="nav-link" to="/about us">About Us</RouterLink>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<RouterView />
+
+</template>
+
+<script script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+
+
+</script>

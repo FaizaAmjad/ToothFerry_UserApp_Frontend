51c95b4f641612e9d9ec62f4463f7b5bff2f7506
David Hong <davidbor@net.chalmers.se>
2023-11-22 21:10:52 +0100

    #19 Add a method for date and time selection  1. Users need to select a specific date and time to make a reservation.  Therefore, I created a method to select specific date and time.  What I wanted to do was using a b-form-datepicker from the bootstrapvue library.  However, for some reason it did not work so I just went with the same mechanism I used for the time selection.  If it is possible to change, the mechanism might be changed.
     2. When the clinics are closed (ex. weekend, lunch time), it should not be
     possible to be booked. Therefore, for now I created a JavaScript function that prohibits
     specific date and time.
     However, the method to forbid dupplicate booking has not been created yet.
     This will be implemented once the MQTT part has been finished.

--- user-App/src/main.js
+++ user-App/src/main.js
@@ -1,8 +1,11 @@
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import BootstrapVue from 'bootstrap-vue-3'
+
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
 const app = createApp(App)
 
--- user-App/src/views/Appointment.vue
+++ user-App/src/views/Appointment.vue
@@ -4,16 +4,113 @@
         <label>Reservation</label>
         <div class="content">
           <div class="left-section">
-            <button>Regular</button>
+            <b-button size="lg" @click="showDateTimePicker">Regular</b-button>
+            <b-modal v-model="dateTimePickerVisible" @ok="saveDateTime" title="Select Date and Time">
+              <b-container>
+                <b-row>
+                  <b-col>
+                    <b-form-group label="Select Date">
+                      <b-form-select v-model="selectedDate" :options="dateSlots" required></b-form-select>
+                    </b-form-group>
+                  </b-col>
+                  <b-col>
+                    <b-form-group label="Select Time">
+                      <b-form-select v-model="selectedTime" :options="timeSlots" required></b-form-select>
+                    </b-form-group>
+                  </b-col>
+                </b-row>
+              </b-container>
+            </b-modal>
           </div>
           <div class="right-section">
-            <button>Emergency</button>
+            <b-button size="lg" href="/questionnaire">Emergency</b-button>
+            
           </div>
         </div>
       </div>
     </div>
   </template>
   
+<script>
+
+
+export default {
+  data() {
+    return {
+      selectedDate: null,
+      selectedTime: null,
+      dateTimePickerVisible: false,
+      timeSlots: generateTimeSlots(),
+      dateSlots: generateDateSlots(),
+    };
+  },
+  methods: {
+    showDateTimePicker() {
+      this.dateTimePickerVisible = true;
+    },
+    hideDateTimePicker() {
+      this.dateTimePickerVisible = false;
+    },
+    selectTime(timeSlot) {
+      this.selectedTime = timeSlot;
+      this.hideDateTimePicker();
+    },
+
+    // Just used a method I got from using ChatGPT for now. This data will be transmitted to the booking_service component without being saved.
+    saveDateTime() {
+      const dataToSave = {
+        selectedDate: this.selectedDate,
+        selectedTime: this.selectedTime,
+      };
+      const jsonString = JSON.stringify(dataToSave, null, 2);
+      const blob = new Blob([jsonString], { type: 'application/json' });
+      const link = document.createElement('a');
+      link.href = URL.createObjectURL(blob);
+      link.download = 'selectedDateTime.json';
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    },
+  },
+};
+
+function generateTimeSlots() {
+  const timeSlots = [];
+  const startTime = new Date();
+  startTime.setHours(8, 0, 0, 0); // Set to the clinic's opening time
+
+  for (let i = 0; i < 24; i++) { // Change the maximum i value to alter the closing time.
+    const time = new Date(startTime.getTime() + i * 30 * 60 * 1000); // Currently have time slots with 30 minutes interval.
+    const formattedTime = time.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+
+    // A method to disable the time slots that are not available (such as reserved or lunch time)
+    const isDisabled = time.getHours() === 12 || (time.getHours() === 13 && time.getMinutes() === 0);
+    timeSlots.push({ text: formattedTime, value: formattedTime, disabled: isDisabled });
+  }
+
+  return timeSlots;
+}
+
+function generateDateSlots() {
+  const dateSlots = [];
+  const currentDate = new Date();
+  currentDate.setHours(0, 0, 0, 0); // Set to the beginning of the day
+
+  for (let i = 0; i < 20; i++) { // Change the maximum i value to alter the number of days ahead
+    const date = new Date(currentDate.getTime() + i * 24 * 60 * 60 * 1000); // Add i days
+    const formattedDate = date.toLocaleDateString('en-US', { weekday: 'short', month: 'short', day: 'numeric' });
+
+    // A method to disable the date slots that are not available (customize as needed)
+    const isDisabled = date.getDay() === 0; // You can implement logic to disable specific dates
+    dateSlots.push({ text: formattedDate, value: date.toISOString(), disabled: isDisabled });
+  }
+
+  return dateSlots;
+}
+
+  
+</script>
+
   <style>
   .reservation {
     margin: 0;
@@ -60,11 +157,5 @@
   align-items: center;
   }
 
-  .content button {
-    font-size: 1.5em; /* Adjust the font size of the buttons */
-    padding: 20px 35px;
-    border-radius: 10px;
-    border-color: #c4ced6;
-  }
   </style>
-  
+ #c4ced6; 

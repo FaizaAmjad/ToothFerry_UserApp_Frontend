5ef3f31f0e825e4fc4a129dc6ba4ff2505855cdf
tehreem <gusasite@student.gu.se>
2023-11-26 00:43:55 +0100

    #24 Enhances the about page and relative information Enhances the formatting of the about page to make it more apealing to the user. It also adds the relevant description and information for the users.

--- user-App/src/views/AboutView.vue
+++ user-App/src/views/AboutView.vue
@@ -1,10 +1,13 @@
 <template>
+   <div id="app">
+    <header>
+      <NavBar/>
+    </header>
+    <router-view/>
+
   <div class="background">
-   
    <div class="container"> 
     <div class="about">
-      <h1>This is an about page</h1>
-
       <h1>About Tooth Fairy Dentist Hub</h1>
       <h3>Welcome to our world of dental excellence and care.</h3>
       
@@ -13,47 +16,47 @@
       </p>
      
       <div class="about-text">
-         <p>🦷 Our Mission:</p>
-         <p>
-           Connected to numerous clinics, Tooth Fairy simplifies the appointment process. Dentists can manage their schedules, view bookings, receive notifications, and seamlessly handle cancellations.
-         </p> 
-      
-      
-         <p>💙 Dentist-Focused Features:</p>
-         <p>
-           This platform empowers dentists to showcase availability, manage appointments, and attend to emergency cases. A unified hub for convenient control and a touch of professional magic.
-         </p>
+        <p>🏥 Explore Clinics:</p>
+            <p>
+              Discover and connect with numerous dental clinics in Gothenburg. Find the one that suits your needs and book appointments effortlessly.
+            </p>
 
-         <p>🌟 Why Choose Us:</p>
-         <p>
-           At Tooth Fairy, we prioritize the comfort and efficiency of our dentists. Experience the ease of having everything in one place, designed with care for our dental professionals.
-         </p>
+            <p>🦷 User-Focused Features:</p>
+            <p>
+              We empower users to browse clinic information, book appointments, and even handle emergency cases. Your dental care, your way.
+            </p>
 
-         <p>🔗 Get Started:</p>
-         <p> 
-          Join Tooth Fairy Dentist Hub and discover a world of streamlined dental practice management!
-         </p>
+            <p>🌟 Why Choose Us:</p>
+            <p>
+              Experience the convenience of having all dental clinics in one place. We prioritize your comfort and efficient access to quality dental care.
+            </p>
 
-         <p>
-          Because your success is our priority. 🌈✨
-         </p>
+            <p>🔗 Get Started:</p>
+            <p>
+              Join Tooth Fairy Dentist Hub and embark on a journey to simplified dental care. Your healthy smile begins here.
+            </p>
+
+            <p>
+              Because your dental well-being matters to us. 🌈✨
+            </p>
 
       </div> 
      </div>
     </div>
   </div>
+</div>
 </template>
 
 <style scoped>
 .background{
-  background-color: rgb(220, 212, 244);
+  background-color: rgb(108, 167, 226);
   padding: 3%;
   min-height: 100vh;
   box-sizing: border-box;
 
 }
 .container{
-  background-color: rgb(191, 178, 226);
+  background-color: rgb(220, 233, 243);
   border-radius: 20px;
 }
 .about {
@@ -84,13 +87,6 @@
   font-weight: 800;
 }
 
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
 
 </style>
 

e0d6d70d470582c204f0eb08b5c7f70aef67d6e0
Faiza Amjad <faizaam@student.chalmers.se>
2023-11-26 01:17:42 +0100

    #16 Add landing page

--- user-App/src/router/index.js
+++ user-App/src/router/index.js
@@ -2,12 +2,18 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Questionnaire from '../views/emergencyForm.vue'
 import AppointmentView from '../views/AppointmentView.vue'
+import LandingView from '../views/LandingView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
+      name: 'landing',
+      component: LandingView
+    },
+    {
+      path: '/home',
       name: 'home',
       component: HomeView
     },
---
+++ user-App/src/views/LandingView.vue
@@ -0,0 +1,31 @@
+<template>
+    
+     <img
+        src="../assets/Tooth-Ferry.jpg"
+        class="min-vh-100 container"
+        fluid-grow
+        alt="Responsive image"/>
+
+  </template>
+  
+  <script>
+  export default {
+    components: {
+    },
+  }
+  </script>
+  
+  <style scoped>
+  .container {
+    background-color: rgb(61, 77, 108);
+    text-align: center;
+    text-shadow: 4px 4px 8px rgb(255, 255, 255);
+    width: 100%;
+    height: 100%;
+    padding: 1%;
+  }
+  .min-vh-100 {
+    opacity: 0.9;
+    object-fit: cover;
+  }
+  </style>

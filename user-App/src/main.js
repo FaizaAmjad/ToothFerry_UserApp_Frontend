065a98fb0214f4af96eac027e48d30cf59c27ba3
ibrah <gusalgaah@student.gu.se>
2023-11-28 15:55:25 +0100

    "resolve merge conflict"

--- user-App/src/main.js
+++ user-App/src/main.js
@@ -1,6 +1,28 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import BootstrapVue3 from 'bootstrap-vue-3'
+import VueGoogleMaps from '@fawmi/vue-google-maps';
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+
+import { API_KEY } from './api_key.js';
+const app = createApp(App)
+
+app.use(BootstrapVue3)
+app.use(router)
+
+app.use(VueGoogleMaps, {
+  load: {
+    key: API_KEY,
+  },
+})
+
+app.mount('#app') 
+import { createApp } from 'vue';
+import App from './App.vue'
+import router from './router'
 import BootstrapVue from 'bootstrap-vue-3'
 
 
@@ -9,10 +31,10 @@ import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 import store from './vuex'
 import './axios'
 
-import { API_KEY } from './api_key.js';
-const app = createApp(App)
 
-app.use(BootstrapVue3)
+
 app.use(router)
+app.use(store)
+app.use(BootstrapVue)
 
 app.mount('#app')

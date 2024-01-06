f21a694af5eca15358ea61a86e633ae359649985
David Hong <davidbor@net.chalmers.se>
2024-01-06 14:00:19 +0100

    #28 Redesign the emergency form
     ## Changes Regarding the Emergency Score
     A completely different approach has been made for the calculation of the emergency score.
     Instead of the server side receiving the survey information, the client's computer does the math and just sends the score.
     This is due to the GDPR issue and by this approach, we never receive any data regarded to personal information.
    
     ## Changes Regarding the Emergency Form
     - Due to the score calculation.
     The previous design did not have an efficient way to store the answers.
     Therefore, I changed the survey's structure so that the question number and the answers are mapped.
     - Due to an unrecognized error
     For some reason, the questions were visible on the BootstrapVue homepage's playground but it wasn't on my pc.
     Therefore, I changed the whole structure.

--- src/views/emergencyForm.vue
+++ src/views/emergencyForm.vue
@@ -1,208 +1,216 @@
 <template>
-    <div class="wrapper inner">
-      <div class="explannation">
-        <h4>Please answer following questionnaire.</h4><br/>
-      </div>
-      <b-form-group label="1. When did you last visit a dentist?" v-slot="{ ariaDescribedby }">
-        <b-form-checkbox-group
-          id="checkbox-group-2"
-          v-model="selected"
-          :aria-describedby="ariaDescribedby"
-          name="flavour-2"
-        >
-          <b-form-checkbox value="option1">6 months or less</b-form-checkbox>
-          <b-form-checkbox value="option2">More than 6 months, but not more than 1 year ago</b-form-checkbox>
-          <b-form-checkbox value="option3">More than 1 year, but not more than 3 years ago</b-form-checkbox>
-          <b-form-checkbox value="option4">More than 3 years, but not more than 5 years ago</b-form-checkbox>
-          <b-form-checkbox value="option5">More than 5 years ago</b-form-checkbox>
-        </b-form-checkbox-group>
-      </b-form-group>
-
-      <b-form-group label="2. Why did you visit the dentist?" v-slot="{ ariaDescribedby }">
-        <b-form-checkbox-group
-          id="checkbox-group-2"
-          v-model="selected"
-          :aria-describedby="ariaDescribedby"
-          name="flavour-2"
-        >
-          <b-form-checkbox value="option1">Personally visited for check-up, examination, or cleaning</b-form-checkbox>
-          <b-form-checkbox value="option2">Was called by the dentist for check-up, examination, or cleaning</b-form-checkbox>
-          <b-form-checkbox value="option3">The previous treatment had a problem</b-form-checkbox>
-          <b-form-checkbox value="option4">Something was wrong, bothering or hurting</b-form-checkbox>
-          <b-form-checkbox value="option5">Went for a treatment of a condition that the dentist discovered at earlier checkup or examination</b-form-checkbox>
-        </b-form-checkbox-group>
-      </b-form-group>
-
-      <b-form-group label="3. How often did you hacve aching in mouth?" v-slot="{ ariaDescribedby }">
-        <b-form-checkbox-group
-          id="checkbox-group-2"
-          v-model="selected"
-          :aria-describedby="ariaDescribedby"
-          name="flavour-2"
-        >
-          <b-form-checkbox value="option1">Never</b-form-checkbox>
-          <b-form-checkbox value="option2">Hardly ever</b-form-checkbox>
-          <b-form-checkbox value="option3">Occasionally</b-form-checkbox>
-          <b-form-checkbox value="option4">Fairly often</b-form-checkbox>
-          <b-form-checkbox value="option5">Very often</b-form-checkbox>
-        </b-form-checkbox-group>
-      </b-form-group>
-
-      <b-form-group label="4. How often did you have difficulty in your job due to oral problem?" v-slot="{ ariaDescribedby }">
-        <b-form-checkbox-group
-          id="checkbox-group-2"
-          v-model="selected"
-          :aria-describedby="ariaDescribedby"
-          name="flavour-2"
-        >
-          <b-form-checkbox value="option1">Never</b-form-checkbox>
-          <b-form-checkbox value="option2">Hardly ever</b-form-checkbox>
-          <b-form-checkbox value="option3">Ocassionally</b-form-checkbox>
-          <b-form-checkbox value="option4">Fairly often</b-form-checkbox>
-          <b-form-checkbox value="option5">Very often</b-form-checkbox>
-        </b-form-checkbox-group>
-      </b-form-group>
-
-      <b-form-group label="5. How would you rate the health of your teeth and gums?" v-slot="{ ariaDescribedby }">
-        <b-form-checkbox-group
-          id="checkbox-group-2"
-          v-model="selected"
-          :aria-describedby="ariaDescribedby"
-          name="flavour-2"
-        >
-          <b-form-checkbox value="option1">Excellent</b-form-checkbox>
-          <b-form-checkbox value="option2">Very Good</b-form-checkbox>
-          <b-form-checkbox value="option3">Good</b-form-checkbox>
-          <b-form-checkbox value="option4">Fair</b-form-checkbox>
-          <b-form-checkbox value="option5">Poor</b-form-checkbox>
-        </b-form-checkbox-group>
-      </b-form-group>
-
-      <b-form-group label="6. Have you ever needed to visit a dentist but could not during the last 12 months?" v-slot="{ ariaDescribedby }">
-        <b-form-checkbox-group
-          id="checkbox-group-2"
-          v-model="selected"
-          :aria-describedby="ariaDescribedby"
-          name="flavour-2"
-        >
-          <b-form-checkbox value="yes">Yes</b-form-checkbox>
-          <b-form-checkbox value="no">No</b-form-checkbox>
-        </b-form-checkbox-group>
-      </b-form-group>
-
-      <b-form-group label="7. Did the dentist explain about the benefit og giving up cigarettes?" v-slot="{ ariaDescribedby }">
-        <b-form-checkbox-group
-          id="checkbox-group-2"
-          v-model="selected"
-          :aria-describedby="ariaDescribedby"
-          name="flavour-2"
-        >
-          <b-form-checkbox value="yes">Yes</b-form-checkbox>
-          <b-form-checkbox value="no">No</b-form-checkbox>
-        </b-form-checkbox-group>
-      </b-form-group>
-
-      <b-form-group label="8. Did the dentist explain about the benefits of checking blood sugar?" v-slot="{ ariaDescribedby }">
-        <b-form-checkbox-group
-          id="checkbox-group-2"
-          v-model="selected"
-          :aria-describedby="ariaDescribedby"
-          name="flavour-2"
-        >
-          <b-form-checkbox value="yes">Yes</b-form-checkbox>
-          <b-form-checkbox value="no">No</b-form-checkbox>
-        </b-form-checkbox-group>
-      </b-form-group>
-
-      <b-form-group label="9. Do you think you might have a gum disease?" v-slot="{ ariaDescribedby }">
-        <b-form-checkbox-group
-          id="checkbox-group-2"
-          v-model="selected"
-          :aria-describedby="ariaDescribedby"
-          name="flavour-2"
-        >
-          <b-form-checkbox value="yes">Yes</b-form-checkbox>
-          <b-form-checkbox value="no">No</b-form-checkbox>
-        </b-form-checkbox-group>
-      </b-form-group>
-
-      <b-form-group label="10. Have you experienced teeth becoming loose without an injury?" v-slot="{ ariaDescribedby }">
-        <b-form-checkbox-group
-          id="checkbox-group-2"
-          v-model="selected"
-          :aria-describedby="ariaDescribedby"
-          name="flavour-2"
-        >
-          <b-form-checkbox value="yes">Yes</b-form-checkbox>
-          <b-form-checkbox value="no">No</b-form-checkbox>
-        </b-form-checkbox-group>
-      </b-form-group>
-  
-      <b-button variant="primary" size="lg" @click="submitAnswer">Submit</b-button>
+  <div>
+    <div class="explannation">
+      <br />
+      <h4>Please answer following questionnaire.</h4>
+      <h6>We require you to answer all of the questions</h6>
+      <br />
     </div>
-  </template>
+    <div class="wrapper inner">
+      <div v-for="(question, index) in multipleChoiceQuestions" :key="index" class="mb-4">
+        <h5>{{ question.text }}</h5>
+        <b-form-group v-if="question.type === 'multipleChoice'">
+          <b-form-radio-group
+            v-model="question.answer"
+            :options="question.choices"
+          ></b-form-radio-group>
+        </b-form-group>
+      </div>
+
+      <div v-for="(question, index) in yesNoQuestions" :key="index" class="mb-4">
+        <h5>{{ question.text }}</h5>
+        <b-form-group v-if="question.type === 'yesNo'">
+          <b-form-radio-group
+            v-model="question.answer"
+            :options="yesNoOptions"
+          ></b-form-radio-group>
+        </b-form-group>
+      </div>
+
+      <b-alert variant="danger" :show="showAlert" @dismiss="showAlert = false">
+        Please answer all questions before submitting the survey.
+      </b-alert>
+      <b-button @click="submitSurvey" variant="primary">Submit</b-button>
+    </div>
+  </div>
+</template>
 
 <script>
+import { postScore } from '../apis/emergencyBooking'
 export default {
-    data() {
+  data() {
     return {
-      selected: [],
-    };
+      showAlert: false,
+      multipleChoiceQuestions: [
+        {
+          text: '1. When did you last visit a dentist',
+          type: 'multipleChoice',
+          choices: [
+            '6 months or less',
+            'More than 6 months, but not more than 1 year ago',
+            'More than 1 year, but not more than 3 years ago',
+            'More than 3 years, but not more than 5 years ago',
+            'More than 5 years ago'
+          ],
+          answer: null
+        },
+        {
+          text: '2. Why did you visit the dentist?',
+          type: 'multipleChoice',
+          choices: [
+            'Personally visited for check-up, examination, or cleaning',
+            'Was called by the dentist for check-up, examination, or cleaning',
+            'The previous treatment had a problem',
+            'Something was wrong, bothering or hurting',
+            'Went for a treatment of a condition that the dentist discovered at earlier checkup or examination'
+          ],
+          answer: null
+        },
+        {
+          text: '3. How often did you have aching in mouth?',
+          type: 'multipleChoice',
+          choices: ['Never', 'Hardly Ever', 'Occassionally', 'Fairly Often', 'Very Often'],
+          answer: null
+        },
+        {
+          text: '4. How often did you have difficulty in your job due to oral problem?',
+          type: 'multipleChoice',
+          choices: ['Never', 'Hardly Ever', 'Occassionally', 'Fairly Often', 'Very Often'],
+          answer: null
+        },
+        {
+          text: '5. How would you rate the health of your teeth and gums?',
+          type: 'multipleChoice',
+          choices: ['Never', 'Hardly Ever', 'Occassionally', 'Fairly Often', 'Very Often'],
+          answer: null
+        }
+      ],
+      yesNoQuestions: [
+        {
+          text: '6. Have you ever needed to visit a dentist but could not during the last 12 months?',
+          type: 'yesNo',
+          answer: null
+        },
+        {
+          text: '7. Did the dentist explain about the benefit og giving up cigarettes?',
+          type: 'yesNo',
+          answer: null
+        },
+        {
+          text: '8. Did the dentist explain about the benefits of checking blood sugar?',
+          type: 'yesNo',
+          answer: null
+        },
+        {
+          text: '9. Do you think you might have a gum disease?',
+          type: 'yesNo',
+          answer: null
+        },
+        {
+          text: '10. Have you experienced teeth becoming loose without an injury?',
+          type: 'yesNo',
+          answer: null
+        }
+      ],
+      yesNoOptions: [
+        { text: 'Yes', value: true },
+        { text: 'No', value: false }
+      ]
+    }
   },
   methods: {
-    submitAnswer() {
-      // Collect the answers in an object
-      const answers = {
-        visitDentist: this.selected[0] || null,
-        visitReason: this.selected[1] || null,
-        achingInMouth: this.selected[2] || null,
-        jobDifficultyDueToOralProblem: this.selected[3] || null,
-        teethAndGumsHealth: this.selected[4] || null,
-        unableToVisitDentist: this.selected[5] || null,
-        dentistExplainedCigarettes: this.selected[6] || null,
-        dentistExplainedBloodSugar: this.selected[7] || null,
-        gumDiseasePossibility: this.selected[8] || null,
-        looseTeethWithoutInjury: this.selected[9] || null,
-      };
-
-      // Convert the object to JSON
-      const jsonData = JSON.stringify(answers, null, 2);
-
-      // Create a Blob containing the JSON data
-      const blob = new Blob([jsonData], { type: 'application/json' });
-
-      // Create a download link
-      const link = document.createElement('a');
-      link.href = URL.createObjectURL(blob);
-      link.download = 'questionnaire_answers.json';
-
-      // Append the link to the body and trigger the download
-      document.body.appendChild(link);
-      link.click();
-
-      // Remove the link from the DOM
-      document.body.removeChild(link);
+    mapOptionToValue(question, option) {
+      if (question.type === 'yesNo') {
+        return option ? 1 : 2
+      }
+      if (question.choices && question.choices.length > 0) {
+        const index = question.choices.findIndex((choice) => choice === option)
+        return index !== -1 ? index + 1 : null
+      }
+      return null
     },
-  },
+    async submitSurvey() {
+      const showAlert = this.multipleChoiceQuestions
+        .concat(this.yesNoQuestions)
+        .some((question) => question.answer === null)
+
+      if (showAlert) {
+        this.showAlert = true
+        return
+      }
+
+      this.showAlert = false
+
+      /* Real survey data but not used for now.
+      const surveyData = {
+        multipleChoiceQuestions: this.multipleChoiceQuestions.map((question, index) => ({
+          questionNumber: index + 1,
+          chosenOption: this.mapOptionToValue(question, question.answer)
+        })),
+        yesNoQuestions: this.yesNoQuestions.map((question, index) => ({
+          questionNumber: index + 1 + this.multipleChoiceQuestions.length,
+          chosenOption: this.mapOptionToValue(question, question.answer)
+        }))
+      }
+      **/
+
+      // Data below is the calculation method for the emergency score.
+      // Instead of our server doing the calculation, the client's computer does the math. (GDPR issue)
+
+      const importanceValues = {
+        1: 22,
+        2: 15,
+        3: 6,
+        4: 6,
+        5: 22,
+        6: 15,
+        7: 2,
+        8: 6,
+        9: 6,
+        10: 6
+      }
+
+      const multipleWeights = {
+        choice1: 0.0,
+        choice2: 0.1,
+        choice3: 0.2,
+        choice4: 0.3,
+        choice5: 0.4
+      }
+
+      const yesNoWeights = {
+        choice1: 1.0,
+        choice2: 0.0
+      }
+
+      const multipleScore = this.multipleChoiceQuestions.reduce((sum, question) => {
+        const chosenOption = this.mapOptionToValue(question, question.answer)
+        const importance = importanceValues[question.text.charAt(0)]
+        const weight = chosenOption !== null ? multipleWeights[`choice${chosenOption}`] : 0
+
+        return sum + importance * weight
+      }, 0)
+
+      const yesNoScore = this.yesNoQuestions.reduce((sum, question) => {
+        const chosenOption = this.mapOptionToValue(question, question.answer)
+
+        const questionNumber = parseInt(question.text.match(/\d+/)[0])
+        const importance = importanceValues[questionNumber]
+        const weight = chosenOption !== null ? yesNoWeights[`choice${chosenOption}`] : 0
+
+        return sum + importance * weight
+      }, 0)
+      const calculatedScore = Number((multipleScore + yesNoScore).toFixed(2))
+
+      console.log(calculatedScore)
+
+      try {
+        const data = await postScore(calculatedScore)
+        this.$router.push('/emergency-results')
+      } catch (err) {
+        this.err = 'An error occured'
+      }
+    }
+  }
 }
 </script>
-
-<style>
-.inner {
-    margin-left: 150px;
-    margin-right: 150px;
-    margin-top: 60px;
-    margin-bottom: 60px;
-    background: #ffffff;
-    padding: 40px;
-    border-radius: 15px;
-    transition: all .3s;
-    text-align: left;
-}
-.explannation {
-    display: flex;
-    align-content: center;
-    justify-content: center;
-    padding: 40px;
-}
-</style>
